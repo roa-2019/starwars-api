556db14a96f0c5927f8e6ffb577fceeaf8c0611e
mariawenrose <42329353+mariawenrose@users.noreply.github.com>
2019-09-18 21:57:00 +1200

    displayData componenet now isolated

--- client/components/DisplayData.jsx
+++ client/components/DisplayData.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
+import { getSatellite } from '../apiClient'
+const issId = 25544
 
-const DisplayData = (props) => {
-  console.log(props)
 
-  return (
-    // <>
-    //   <h1>International Space Station</h1>
-    //   <p>Lat: {props.latitude}</p>
-    //   <p>Lng: {props.longitude}</p>
-    //   <button onClick={this.handleClick}>Refresh</button>
-    // </>
-    null
-  )
+class DisplayData extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      satellite: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getSatelliteData()
+  }
+
+
+  handleClick = (e) => {
+    e.preventDefault()
+    this.getSatelliteData()
+  }
+
+
+  getSatelliteData = () => {
+    return getSatellite(issId)
+      .then(satellite => {
+        this.setState({
+          satellite: JSON.parse(satellite.text)
+        })
+      })
+  }
+
+
+  render() {
+
+    const data = this.state.satellite
+
+    return (
+      <>
+        <h1>International Space Station</h1>
+        <p>Lat: {data.latitude}</p>
+        <p>Lng: {data.longitude}</p>
+        <button onClick={this.handleClick}>Refresh</button>
+      </>
+    )
+  }
 }
 
 export default DisplayData
-
